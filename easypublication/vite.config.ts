bdfead4cc7ebd546a4b988f5fd59bd73bde0352b
Zoheb Sharif <sharifzoheb@gmail.com>
2025-08-01 11:13:50 -0700

    Update build config to properly handle data files

--- easypublication/vite.config.ts
+++ easypublication/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
@@ -9,10 +10,19 @@ export default defineConfig(({ command }) => ({
   server: {
     proxy: {
       '/api': {
-        target: command === 'serve' ? 'http://localhost:3001' : 'https://easypublication-server.onrender.com',
+        target: command === 'serve' ? 'http://localhost:3001' : 'https://easypublication.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  },
+  publicDir: 'public'
 }))
