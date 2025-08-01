f75cbca06650fd55e1466b9de676584f06feff1e
Zoheb Sharif <sharifzoheb@gmail.com>
2025-08-01 11:41:25 -0700

    Recreate source files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Admin from "./Admin";
+import SlideshowView from "./SlideshowView";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/slideshow" element={<SlideshowView />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/SlideshowView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SlideshowView() {
+  return (
+    <div>
+      <h1>Slideshow</h1>
+      <p>Slideshow view coming soon</p>
+    </div>
+  );
+}
+
+export default SlideshowView;
