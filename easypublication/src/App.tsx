2e2092095351cad4a43d239c90ec8db68c436381
Zoheb Sharif <sharifzoheb@gmail.com>
2025-08-01 11:10:04 -0700

    Switch to HashRouter for GitHub Pages compatibility

--- easypublication/src/App.tsx
+++ easypublication/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import Admin from "./Admin";
 import SlideshowView from "./SlideshowView";
